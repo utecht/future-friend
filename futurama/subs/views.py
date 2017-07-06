3db2430c0cf3b367ea91d758b37be78dc6b8292b
Joseph Utecht <thenmal@gmail.com>
2017-07-06 19:44:26 +0000

    moved to new gif encoding scheme

--- futurama/subs/views.py
+++ futurama/subs/views.py
@@ -36,7 +36,7 @@ def create_media(request, id, file_format):
     font_size = int((28 / len(max(sub.line.split('\n'), key=len))) * 50)
     if font_size > 50:
         font_size = 50
-    if file_format == 'bgif':
+    if file_format == 'gif':
         args = []
         args.append('ffmpeg')
         args.append('-y')
@@ -47,37 +47,52 @@ def create_media(request, id, file_format):
         args.append('-i')
         args.append('"{}"'.format(sub.episode.episode_file))
         args.append('-vf')
-        args.append("fps=15,palettegen")
+        args.append('fps=15,drawtext="fontfile=/usr/share/fonts/TTF/impact.ttf:textfile={}:fontcolor=white:fontsize={}:x=(w-text_w)/2:y=((h*1.75)-text_h)/2:borderw=3"'.format(text_file, font_size))
+        args.append('"{}{}.mp4"'.format(settings.MEDIA_ROOT, name))
+        command = ' '.join(args)
+        p = subprocess.Popen(command, shell=True)
+        p.wait()
+        args = []
+        args.append('ffmpeg')
+        args.append('-y')
+        args.append('-i')
+        args.append('"{}{}.mp4"'.format(settings.MEDIA_ROOT, name))
+        args.append('-vf')
+        args.append("palettegen")
         args.append('{}{}_palette.png'.format(settings.MEDIA_ROOT, name))
         command = ' '.join(args)
         p = subprocess.Popen(command, shell=True)
         p.wait()
-
-    args = []
-    args.append('ffmpeg')
-    args.append('-y')
-    args.append('-ss')
-    args.append(sub.start)
-    args.append('-t')
-    args.append(sub.duration)
-    args.append('-i')
-    args.append('"{}"'.format(sub.episode.episode_file))
-    if file_format == 'bgif':
+        args = []
+        args.append('ffmpeg')
+        args.append('-y')
+        args.append('-i')
+        args.append('"{}{}.mp4"'.format(settings.MEDIA_ROOT, name))
         args.append('-i')
         args.append('"{}{}_palette.png"'.format(settings.MEDIA_ROOT, name))
-    if file_format == 'webm':
-        args.append('-strict')
-        args.append('-2')
-    if file_format == 'bgif':
         args.append('-filter_complex')
-        args.append("\"fps=15 [x]; [x][1:v] paletteuse,drawtext='fontfile=/usr/share/fonts/TTF/impact.ttf:textfile={}:fontcolor=white:fontsize={}:x=(w-text_w)/2:y=((h*1.75)-text_h)/2:borderw=3'\"".format(text_file, font_size))
-        file_format = 'gif'
+        args.append('fps=15,scale=480:-1,paletteuse')
+        args.append('"{}{}.{}"'.format(settings.MEDIA_ROOT, name, file_format))
+        command = ' '.join(args)
+        p = subprocess.Popen(command, shell=True)
+        p.wait()
     else:
+        args = []
+        args.append('ffmpeg')
+        args.append('-y')
+        args.append('-ss')
+        args.append(sub.start)
+        args.append('-t')
+        args.append(sub.duration)
+        args.append('-i')
+        args.append('"{}"'.format(sub.episode.episode_file))
+        if file_format == 'webm':
+            args.append('-strict')
+            args.append('-2')
         args.append('-vf')
         args.append('drawtext="fontfile=/usr/share/fonts/TTF/impact.ttf:textfile={}:fontcolor=white:fontsize={}:x=(w-text_w)/2:y=((h*1.75)-text_h)/2:borderw=3"'.format(text_file, font_size))
-    args.append('"{}{}.{}"'.format(settings.MEDIA_ROOT, name, file_format))
-    command = ' '.join(args)
-    print(command)
-    p = subprocess.Popen(command, shell=True)
-    p.wait()
+        args.append('"{}{}.{}"'.format(settings.MEDIA_ROOT, name, file_format))
+        command = ' '.join(args)
+        p = subprocess.Popen(command, shell=True)
+        p.wait()
     return redirect('{}{}.{}'.format(settings.MEDIA_URL, name, file_format))
